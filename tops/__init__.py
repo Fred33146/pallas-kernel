8f3c8078afc8d0aff687bf98d13e1316585c426c
0xaskr <mail@askr.dev>
2026-03-24 22:36:34 +0800

    Release branch (#58)
    * feat: add __version__ support to tops package
    
    * chore: bump version to 0.1.1

---
+++ tops/__init__.py
@@ -0,0 +1,8 @@
+from importlib.metadata import version, PackageNotFoundError
+
+try:
+    __version__ = version("tops")
+except PackageNotFoundError:
+    __version__ = "unknown"
+
+__all__ = ["__version__"]
